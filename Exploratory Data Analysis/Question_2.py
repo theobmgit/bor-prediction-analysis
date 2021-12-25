a5b40880ddc088f58213cbd9c518c5ced6dd0e44
Trung <trung.ct190098@sis.hust.edu.vn>
2021-12-25 21:37:09 +0700

    Modify Question Vitualization

--- Exploratory Data Analysis/Question_2.py
+++ Exploratory Data Analysis/Question_2.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
 sns.set_theme(style="whitegrid")
+
 data=pd.read_csv('../dataset/processed/cleanedData.csv')
 
 import ast
@@ -12,14 +12,35 @@ for col in cols:
     data[col]=data[col].apply(ast.literal_eval)
 
 ## Question 2
-## How are the number of release by month?
-## 
+## How average gross depend on Release_Month and combine with Release_Year.
+## We see that the movies release in the month [5,6,7,11,12]
+## tend to have higher gross
+cols=['Release_Month','Gross_worldwide']
 
-
-month=data['Release_Month'].value_counts().sort_index().reset_index()
-month=month.rename(columns={"index":"Release_Month","Release_Month":"Count"})
-month.plot.bar(x='Release_Month',y='Count')
-
+month=data[cols]
+month=month.groupby("Release_Month").mean().reset_index()
+#1
+month.plot.bar(x='Release_Month',y='Gross_worldwide')
+plt.title("Average by Month")
+#2
+data.plot.scatter(x='Release_Month',y='Gross_worldwide')
+plt.title("Scatter plot for Release_Month and Gross")
+#3
+rColor=[2,3,4,8,] 
+gColor=[5,6,7,11,12]
+bColor=[1,9,10]
+colorMap=dict()
+for i in rColor:
+    colorMap[i]='tab:red'
+for i in gColor:
+    colorMap[i]='tab:green'
+for i in bColor:
+    colorMap[i]='tab:blue'
+C=colorMap
+fig = plt.figure(figsize=(8,6))
+sns.scatterplot(data=data,x='Release_Year',y='Gross_worldwide',hue='Release_Month',palette=C)
+plt.title("How gross distributed by Month and Year")
+plt.show()
 
 
 
