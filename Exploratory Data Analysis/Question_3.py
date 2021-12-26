8e224ab27417e4b1ffe1c2aede6f14535269477e
Trung <trung.ct190098@sis.hust.edu.vn>
2021-12-26 07:25:35 +0700

    Q3

--- Exploratory Data Analysis/Question_3.py
+++ Exploratory Data Analysis/Question_3.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 sns.set_theme(style="whitegrid")
 
 data=pd.read_csv('../dataset/processed/cleanedData.csv')
-## Correlation Matrix
+## Correlation Matrix and Histogram
 ##  Question 3
 ##
 import ast
 cols =['Cast', 'Genre', 'Studios', 'ListOfCertificate','Keywords', 'Languages', 'Countries']
 for col in cols:
     data[col]=data[col].apply(ast.literal_eval)
-cols=['Budget','Runtime','Release_Year','Gross_worldwide','Rating','Rating_Count']
+cols=['Budget','Runtime','Release_Year','Gross_worldwide','Rating','Rating_Count','Release_Month']
 data=data[cols]
 sns.heatmap(data.corr(),annot=True)
+
+data.hist(bins=50,figsize=(20,15))
