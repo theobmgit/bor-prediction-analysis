56585c6859ca184879549e6b79cd7c0550a220de
Tu Bui <tu.dominic861@gmail.com>
2022-01-04 21:56:51 +0700

    RÃefactor code

---
+++ src/joinData.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Join by Movie_ID
+data = pd.read_csv("../dataset/data.csv")
+data_mpaa = pd.read_csv("../dataset/data_mpaa.csv")
+data2 = data.merge(data_mpaa, on='Movie_ID', how='left', suffixes=('', '_right'))
+data2 = data2.reset_index(drop=True)
+data2.drop('Movie_Title_right', axis=1, inplace=True)
+data2.to_csv("data_joined.csv", index=False)
